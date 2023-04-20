ac063c25e41bc404708dd2346711ca06224ee63f
Mike Hale <mike@stompgear.com>
2023-04-20 00:47:12 -0500

    add certification & test

--- programs/vanward/src/lib.rs
+++ programs/vanward/src/lib.rs
@@ -5,11 +5,44 @@ declare_id!("AKUscwsW639dX2X8YRSes9mqx221HU1ti9ZDGdX6iVYV");
 #[program]
 pub mod vanward {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    // add certification
+    pub fn add_certification(
+        ctx: Context<AddCertification>,
+        id: String,
+        year: u16,
+        title: String,
+        bump: u8,
+    ) -> Result<()> {
+        let cert = &mut ctx.accounts.certification;
+        cert.authority = *ctx.accounts.user.key;
+        cert.id = id;
+        cert.year = year;
+        cert.title = title;
+        cert.bump = bump;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(id: String, year: u16)]
+pub struct AddCertification<'info> {
+    #[account(init, payer = user, space = 256, seeds = [
+        b"certification",
+        id.as_bytes(),
+        year.to_le_bytes().as_ref(),
+        user.to_account_info().key.as_ref(),
+    ], bump)]
+    pub certification: Account<'info, Certification>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Certification {
+    pub authority: Pubkey,
+    pub id: String,
+    pub year: u16,
+    pub title: String,
+    pub bump: u8,
+}
--- tests/vanward.ts
+++ tests/vanward.ts
@@ -1,16 +1,56 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Vanward } from "../target/types/vanward";
+import {
+  Program,
+  AnchorProvider,
+  web3,
+  utils,
+  BN,
+} from '@project-serum/anchor';
+import * as anchor from '@project-serum/anchor';
+import Web3 from 'web3';
+import { expect } from 'chai';
+import { Vanward } from '../target/types/vanward';
 
-describe("vanward", () => {
+describe('vanward', async () => {
+  const certificationId = 'CERT' + (Math.floor(Math.random() * 90000) + 10000);
+  const certificationTitle = 'My Certification for 2024';
+  const certificationYear = 2024;
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.Vanward as Program<Vanward>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const [certificationPda, certBump] =
+    await anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        anchor.utils.bytes.utf8.encode('certification'),
+        anchor.utils.bytes.utf8.encode(certificationId),
+        new anchor.BN(certificationYear).toBuffer('le', 2),
+        provider.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+  it('can add a certification', async () => {
+    const tx = await program.methods
+      .addCertification(
+        certificationId,
+        certificationYear,
+        certificationTitle,
+        certBump
+      )
+      .accounts({
+        certification: certificationPda,
+        user: provider.wallet.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    let certAccount = await program.account.certification.fetch(
+      certificationPda
+    );
+    expect(certAccount.id).equals(certificationId);
+    expect(certAccount.year).equals(certificationYear);
+    expect(certAccount.title).equals(certificationTitle);
   });
 });
